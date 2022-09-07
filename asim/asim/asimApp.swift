b63efc7c693ed06b1d044241292e807885e4b368
yaki <kiwidancebad@gmail.com>
2022-09-08 00:34:03 +0300

    init stuff

---
+++ asim/asim/asimApp.swift
@@ -0,0 +1,17 @@
+//
+//  asimApp.swift
+//  asim
+//
+//  Created by Ivan Gubanov on 08/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct asimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
